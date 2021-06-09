2d480a465602675bbededfd2afa56cc65faf71cd
Henri Bambang <henribambang5@gmail.com>
2021-06-09 20:43:11 +0700

    crud_volley

---
+++ crud_volley/app/src/main/java/com/example/crud_volley/Util/ServerAPI.java
@@ -0,0 +1,12 @@
+package com.example.crud_volley.Util;
+
+/**
+ * Henri Bambang S.
+ */
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.0.235/framework/minggu11/crud/ci3/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.0.235/framework/minggu11/crud/ci3/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.0.235/framework/minggu11/crud/ci3/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.0.235/framework/minggu11/crud/ci3/Mahasiswa/ApiUpdate";
+}
